90e6385f773da251e3424e99035ce96ef82b3e24
srivastava-yash <yash.184040@gmail.com>
2023-06-07 00:40:52 -0400

    env correction

--- api/database/database.go
+++ api/database/database.go
@@ -10,7 +10,7 @@ var Ctx = context.Background()
 
 func CreateClient(dbNo int) *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("DB_ADDR"),
+		Addr:     os.Getenv("DB_ADDRESS"),
 		Password: os.Getenv("DB_PASS"),
 		DB:       dbNo,
 	})
